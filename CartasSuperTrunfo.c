e37eebb26ba4122cc2c6d374809aa9c2717a72c0
luizJuni0r <luizdcarlos21@gmail.com>
2025-06-20 14:30:21 +0000

    Atualizei o código para o desafio aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,7 @@
 int main(){
     char carta1, carta2, nomeCidade1[50], nomeCidade2[50];
     int populacao1, turismo1, populacao2, turismo2, codigo1, codigo2;
-    float area1, pib1, area2, pib2;
+    float area1, pib1, area2, pib2, densidadePo1, densidadePo2, pibperC1, pibperC2;
 
     // Cabeçalho simples e chamativo.
     printf("--------------------------------------------------\n");
@@ -14,7 +14,7 @@ int main(){
 
 
     // As linhas que se seguem coletam os dados inseridos pelo usuário e colocam nas variáveis com terminação "1", para a primeira carta.
-    printf("Esolha uma carta entre A e H para representar o estado\n");
+    printf("Escolha uma carta entre A e H para representar o estado\n");
     printf(">> ");
     scanf(" %c", &carta1);
     printf("\n");
@@ -59,6 +59,12 @@ int main(){
     printf("\n");
     printf("\n");
 
+
+    // Calculo de densidade populacional e PIB per capita da PRIMEIRA CARTA:
+    densidadePo1 = (float)populacao1 / area1;
+    pibperC1 = (float)pib1 / populacao1;
+
+
     // Exibição da primeira carta.
     printf("Carta 01:\n");
     printf("\n");
@@ -66,9 +72,11 @@ int main(){
     printf("Codigo: %c%02d\n", carta1, codigo1);
     printf("Nome da cidade: %s\n", nomeCidade1);
     printf("Populacao: %d\n", populacao1);
-    printf("Area: %.2f\n", area1);
+    printf("Area: %.2fkm2\n", area1);
     printf("PIB: %.2f\n", pib1);
     printf("Numeros de pontos turisticos: %d\n", turismo1);
+    printf("Densidade populacional: %.2f hab/km2\n", densidadePo1);
+    printf("PIB per Capita: %.2f reais\n", pibperC1);
     printf("\n");
 
 
@@ -80,6 +88,8 @@ int main(){
     printf("-----------------------------------------------------\n");
     printf("\n");
     printf("\n");
+
+
 
     // As perguntas se repetem, mas agora armazenaremos os dados nas variáveis com teminação "2", para a segunda carta.
     printf("Escolha uma carta entre A e H para representar o estado.\n");
@@ -127,6 +137,9 @@ int main(){
     printf("\n");
     printf("\n");
 
+    // Calculo de densidade populacional e PIB per capita da SEGUNDA CARTA:
+    densidadePo2 = (float)populacao2 / area2;
+    pibperC2 = (float)pib2 / populacao2;
 
     // Exibição da segunda carta.
     printf("Carta 02:\n");
@@ -135,9 +148,11 @@ int main(){
     printf("Codigo: %c%02d\n", carta2, codigo2);
     printf("Nome da cidade: %s\n", nomeCidade2);
     printf("Populacao: %d\n", populacao2);
-    printf("Area: %.2f\n", area2);
+    printf("Area: %.2fkm2\n", area2);
     printf("PIB: %.2f\n", pib2);
     printf("Numeros de pontos turisticos: %d\n", turismo2);
+    printf("Densidade populacional: %.2f hab/km2\n", densidadePo2);
+    printf("PIB per Capita: %.2f reais\n", pibperC2);
 
     return 0;
 
